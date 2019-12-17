672c03e4bb44f7c699821c8d300001f5a53c12d4
EFSTATHOPOULOS JUAN PABLO <SE25031@grupo.ypf.com>
2019-12-17 02:43:57 -0300

    Municipio controller

--- controllers/municipioController.js
+++ controllers/municipioController.js
@@ -3,7 +3,14 @@ Municipio = require('./../models/municipioModel');
 
 // Handle index actions
 exports.get = function (req, res) {
-    Municipio.find({}).exec((function (err, centros) {
+    Municipio.find({}).sort( { nombre: 1 } ).exec((function (err, municipios) {
+    // Municipio.aggregate([{
+    //     '$project' : {
+    //         _id: false,
+    //         name: "$nombre",
+    //         value: "$cantCentros"
+    //       }
+    // }]).sort( { nombre: 1 } ).exec((function (err, municipios) {
         if (err) {
             res.json({
                 status: "error",
@@ -13,12 +20,12 @@ exports.get = function (req, res) {
         res.json({
             status: "success",
             message: "municipios obtenidas satisfactoriamente",
-            data: centros
+            data: municipios
         });
     }));
 };
 exports.getByNombre = function (req, res) {
-    Municipio.find({nombre:  new RegExp('\\b' +  req.params.nombre + '\\b', 'i')}).exec((function (err, centros) {
+    Municipio.find({ nombre: new RegExp('\\b' + req.params.nombre + '\\b', 'i') }).exec((function (err, municipios) {
         if (err) {
             res.json({
                 status: "error",
@@ -28,7 +35,43 @@ exports.getByNombre = function (req, res) {
         res.json({
             status: "success",
             message: "municipios obtenidas satisfactoriamente",
-            data: centros
+            data: municipios
+        });
+    }));
+};
+
+exports.getByRangoCentros = function (req, res) {
+    let desde = req.params.desde;
+    let hasta = req.params.desde
+    console.log(desde);
+    console.log(hasta);
+    Municipio.find({ cantCentros: { $gt: desde, $lt: hasta } }).exec((function (err, municipios) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+        }
+        res.json({
+            status: "success",
+            message: "municipios obtenidas satisfactoriamente",
+            data: municipios
+        });
+    }));
+};
+
+exports.getSinCentros = function (req, res) {
+    Municipio.find({ cantCentros: 0 }).exec((function (err, municipios) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+        }
+        res.json({
+            status: "success",
+            message: "municipios obtenidas satisfactoriamente",
+            data: municipios
         });
     }));
 };
